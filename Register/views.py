848cc7846c2784bd3efd88200290d3fd6f57be15
MRBOX <olamigokeadebayo90@gmail.com>
2025-08-30 10:39:43 +0100

    Urls

--- Register/views.py
+++ Register/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from rest_framework import generics
 from .models import Course, Registration
 from .serializers import CourseSeraializer, RegistrationSerializers
+from rest_framework.permissions import AllowAny
 
 
 
@@ -10,7 +11,7 @@ from .serializers import CourseSeraializer, RegistrationSerializers
 class CourseListview(generics.ListAPIView):
     queryser = Course.objects.all()
     serializer_class = CourseSeraializer
-    
+    permission_classes = [AllowAny]  
 
 class RegistrationCreateView(generics.CreateAPIView):
     queryset = Registration.objects.all()
